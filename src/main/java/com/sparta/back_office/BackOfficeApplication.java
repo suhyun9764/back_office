05e2cec485e653f427e5715ee6dd6c0aafb0fba7
suhyun9764 <suhyun9764@naver.com>
2024-03-06 11:46:30 +0900

    build(): init commit

---
+++ src/main/java/com/sparta/back_office/BackOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.back_office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackOfficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/back_office/BackOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.back_office;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackOfficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
